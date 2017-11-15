bb1276bca28ed554ebfb04f401d317851d271af5
Kris <krisreid_06@hotmail.com>
2017-11-15 18:10:12 +0000

    Firebase Auth and Firebase Database

--- TBG/SignUpViewController.swift
+++ TBG/SignUpViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
     
-    @IBOutlet weak var tfPlayer: UITextField!
-    @IBOutlet weak var tfManager: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var tfEmailAddress: UITextField!
+    @IBOutlet weak var tfFullName: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var isManager = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,26 +29,49 @@ class SignUpViewController: UIViewController {
     
     @IBAction func scTapped(_ sender: Any) {
         if (segmentedControl.selectedSegmentIndex == 0) {
-            tfManager.isHidden = false
+            isManager = false
+            //set management hidden = false
         } else if (segmentedControl.selectedSegmentIndex == 1) {
-            tfManager.isHidden = true
+            isManager = true
+            //set managment fields to hidden = true
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnSignup(_ sender: Any) {
+        if tfFullName.text == "" || tfPassword.text == "" || tfEmailAddress.text == "" {
+            displayAlert(title: "Missing Information", message: "You must provide both an email and password")
+        } else {
+            if let email = tfEmailAddress.text {
+                if let password = tfPassword.text {
+                    if let fullName = tfFullName.text {
+                        //Create a user in Auth
+                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                                //req?.displayName = fullName
+                                req?.commitChanges(completion: nil)
+                            }
+                        })
+                        
+                        //Add the player to the DB
+                        let playerDictionary : [String:Any] = ["email": email, "fullName": fullName]
+                    Database.database().reference().child("Players").childByAutoId().setValue(playerDictionary)
+                        
+                        self.performSegue(withIdentifier: "signupSubmitSegue", sender: nil)
+                    
+                    }
+                }
+            }
+        }
     }
     
-    //closes the keyboard when you touch white space
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    func displayAlert(title:String, message:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
     }
-    
-    //enter button will close the keyboard
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
+
 
 }
