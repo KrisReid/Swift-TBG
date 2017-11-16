c9b5946bb7e9bd39e2441dc3f8355fe83fd4bc6e
Kris <krisreid_06@hotmail.com>
2017-11-16 18:49:58 +0000

    Updated the fields that write to Firebase.
    Next Phase:
    - Don't use a generated number, but create a new datasource for teams that generates a uniqueID and stores it against the player who is creating the team

--- TBG/SignUpViewController.swift
+++ TBG/SignUpViewController.swift
@@ -66,30 +66,87 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
     }
     
+    func random(_ n: Int) -> String {
+        let a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        var s = ""
+        
+        for _ in 0..<n
+        {
+            let r = Int(arc4random_uniform(UInt32(a.characters.count)))
+            s += String(a[a.index(a.startIndex, offsetBy: r)])
+        }
+        return s
+    }
+    
+    
+    
     @IBAction func btnSignup(_ sender: Any) {
+        
+        
+        
         if tfFullName.text == "" || tfPassword.text == "" || tfEmailAddress.text == "" {
             displayAlert(title: "Missing Information", message: "You must provide both an email and password")
         } else {
             if let email = tfEmailAddress.text {
                 if let password = tfPassword.text {
                     if let fullName = tfFullName.text {
-                        //Create a user in Auth
-                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
-                            if error != nil {
-                                self.displayAlert(title: "Error", message: error!.localizedDescription)
-                            } else {
-                                let req = Auth.auth().currentUser?.createProfileChangeRequest()
-                                //req?.displayName = fullName
-                                req?.commitChanges(completion: nil)
+                        if let address1 = tfAddressLine1.text {
+                            if let address2 = tfAddressLine2.text {
+                                if let postcode = tfPostcode.text {
+                                    if let teamId = tfTeamId.text {
+                                        if let teamName = tfTeamName.text {
+                                            if let teamPostcode = tfTeamPostcode.text {
+                                                if managerSwitch.isOn {
+                                                    //manager switch is on
+                                                    if fullName == "" || password == "" || email == "" || address1 == "" || address2 == "" || postcode == "" || teamName == "" || teamPostcode == "" {
+                                                        displayAlert(title: "Missing Information", message: "You must provide information in all of the fields provided.")
+                                                    } else {
+                                                        //Create a user in Auth
+                                                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                                                            if error != nil {
+                                                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                                                            } else {
+                                                                let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                                                                //req?.displayName = fullName
+                                                                req?.commitChanges(completion: nil)
+                                                            }
+                                                        })
+                                                        
+                                                        //Add the player to the DB
+                                                        let playerDictionary : [String:Any] = ["Email": email, "Full Name": fullName, "Address Line 1": address1, "Address Line 2": address2, "Postcode": postcode, "Team ID": random(14), "Team Name": teamName, "Team Postcode":teamPostcode]
+                                                        Database.database().reference().child("Players").childByAutoId().setValue(playerDictionary)
+                                                        
+                                                        self.performSegue(withIdentifier: "signupSubmitSegue", sender: nil)
+                                                    }
+                                                } else {
+                                                    //manager switch is off
+                                                    if fullName == "" || password == "" || email == "" || address1 == "" || address2 == "" || postcode == "" || teamId == "" {
+                                                        displayAlert(title: "Missing Information", message: "You must provide information in all of the fields provided.")
+                                                    } else {
+                                                        //Create a user in Auth
+                                                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                                                            if error != nil {
+                                                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                                                            } else {
+                                                                let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                                                                //req?.displayName = fullName
+                                                                req?.commitChanges(completion: nil)
+                                                            }
+                                                        })
+                                                        
+                                                        //Add the player to the DB
+                                                        let playerDictionary : [String:Any] = ["Email": email, "Full Name": fullName, "Address Line 1": address1, "Address Line 2": address2, "Postcode": postcode, "Team ID": teamId]
+                                                        Database.database().reference().child("Players").childByAutoId().setValue(playerDictionary)
+                                                        
+                                                        self.performSegue(withIdentifier: "signupSubmitSegue", sender: nil)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             }
-                        })
-                        
-                        //Add the player to the DB
-                        let playerDictionary : [String:Any] = ["email": email, "fullName": fullName]
-                    Database.database().reference().child("Players").childByAutoId().setValue(playerDictionary)
-                        
-                        self.performSegue(withIdentifier: "signupSubmitSegue", sender: nil)
-                    
+                        }
                     }
                 }
             }
