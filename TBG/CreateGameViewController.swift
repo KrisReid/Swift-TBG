65c0de3e7f137ba0e068cbad4053ee14d95d3842
Kris <krisreid_06@hotmail.com>
2017-12-27 18:57:30 +0000

    Create game continued

--- TBG/CreateGameViewController.swift
+++ TBG/CreateGameViewController.swift
@@ -8,24 +8,34 @@
 
 import UIKit
 
-class CreateGameViewController: UIViewController {
+class CreateGameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var btnOpposition: UIButton!
+    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var btnDateOpenClose: UIButton!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var tfDate: UITextField!
     @IBOutlet weak var tfVenue: UITextField!
+    @IBOutlet weak var tfOpposition: UITextField!
     
     var dateMode = true
+    var oppositionMode = true
+    
+    let teams = ["Avonmouth", "Shirehampton", "Welwyn"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func btnCreateGame(_ sender: Any) {
+        print("Clicked")
+    }
+    
     @IBAction func btnDateTapped(_ sender: UIButton) {
         UIView.animate(withDuration: 0.3, animations: {
             self.datePicker.isHidden = !self.datePicker.isHidden
             self.view.layoutIfNeeded()
-            
         })
         
         if dateMode {
@@ -37,15 +47,47 @@ class CreateGameViewController: UIViewController {
         }
     }
     
-
+    @IBAction func btnOppositionClicked(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.pickerView.isHidden = !self.pickerView.isHidden
+            self.view.layoutIfNeeded()
+        })
+        
+        if oppositionMode {
+            oppositionMode = false
+            btnOpposition.setTitle("Clear", for: .normal)
+        } else {
+            oppositionMode = true
+            btnOpposition.setTitle("Pick Team", for: .normal)
+        }
+    }
+    
+    // Code for Date Picker
     @IBAction func datePickerAction(_ sender: Any) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
         let strDate = dateFormatter.string(from: datePicker.date)
 
         self.tfDate.text = String(strDate)
-        
     }
+    
+    //code for Picker View
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return teams.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return teams[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.tfOpposition.text = teams[row]
+    }
+    
     
     //closes the keyboard when you touch white space
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -57,7 +99,5 @@ class CreateGameViewController: UIViewController {
         textField.resignFirstResponder()
         return true
     }
-    
-
 
 }
