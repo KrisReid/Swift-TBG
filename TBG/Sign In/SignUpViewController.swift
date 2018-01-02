4298db645a356a3bd4bc20ed58c8c8f7d2f509e9
Kris <krisreid_06@hotmail.com>
2018-01-02 21:15:20 +0000

    fixed duplicate email error messaging.

--- TBG/Sign In/SignUpViewController.swift
+++ TBG/Sign In/SignUpViewController.swift
@@ -159,127 +159,118 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
             }
         }
     }
-
+    
     
     @IBAction func btnSignup(_ sender: Any) {
-        if let photo = imgProfileImage.image {
-            if let email = tfEmailAddress.text {
-                if let password = tfPassword.text {
-                    if let fullName = tfFullName.text {
-                        if let address1 = tfAddressLine1.text {
-                            if let address2 = tfAddressLine2.text {
-                                if let postcode = tfPostcode.text {
-                                    if let teamId = tfTeamId.text {
-                                        if let teamName = tfTeamName.text {
-                                            if let teamPostcode = tfTeamPostcode.text {
-                                                
-                                                if self.managerSwitch.isOn {
-                                                    //MANAGER MODE
-                                                    if fullName == "" || password == "" || email == "" || address1 == "" || address2 == "" || postcode == "" || teamName == "" || teamPostcode == "" {
-                                                        self.displayAlert(title: "Missing Information", message: "You must provide information in all of the fields provided.")
-                                                    } else {
-                                                        
-                                                        teamNameDBCheck()
-                                                        PostcodeDBCheck()
-                                                        
-                                                        if teamNameExistsInDB && teamPostcodeExistsInDB {
-                                                            self.displayAlert(title: "Team already exists", message: "The team already exists. Please create a different team or contact your club to get the TeamID.")
-                                                        } else {
-                                                            
-                                                            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
-                                                                if let error = error {
-                                                                    self.displayAlert(title: "Error", message: error.localizedDescription)
-                                                                } else {
-                                                                    let req = Auth.auth().currentUser?.createProfileChangeRequest()
-                                                                    req?.displayName = "Manager"
-                                                                    req?.commitChanges(completion: nil)
-                                                                    
-                                                                    if let user = user {
-                                                                        let imageFolder = Storage.storage().reference().child("images")
-                                                                        if let uploadData = UIImageJPEGRepresentation(photo, 0.2) {
-                                                                            imageFolder.child("\(NSUUID().uuidString).jpg").putData(uploadData, metadata: nil, completion: { (metadata, error) in
-                                                                                if let error = error {
-                                                                                    self.displayAlert(title: "Error", message: error.localizedDescription)
-                                                                                }
-                                                                                if let profileImageUrl = metadata?.downloadURL()?.absoluteString  {
-                                                                                    
-                                                                                    //Add the Team to the DB
-                                                                                    let newRef = Database.database().reference().child("Teams").childByAutoId()
-                                                                                    let newKey = newRef.key
-                                                                                    let TeamDictionary : [String:Any] = ["Team Name": teamName, "Team Postcode":teamPostcode, "id": newKey]
-                                                                                    newRef.setValue(TeamDictionary)
-                                                                                    
-                                                                                    //Add the player to the DB
-                                                                                    let playerDictionary : [String:Any] = ["Email": email, "Full Name": fullName, "Address Line 1": address1, "Address Line 2": address2, "Postcode": postcode, "Team ID": newKey, "ProfileImage": profileImageUrl, "Manager": true, "Position":"Defender", "Position Side": "Right"]
-                                                                                    
-                                                                                    Database.database().reference().child("Players").child(user.uid).setValue(playerDictionary)
-                                                                                }
-                                                                            })
-                                                                        }
-                                                                    }
-                                                                }
-                                                            })
-                                                        }
-                                                        //Segue back to the login page
-                                                        self.performSegue(withIdentifier: "signupSubmitSegue", sender: email)
-                                                    }
-                                                } else {
-                                                    //PLAYER MODE
-                                                    
-                                                    
-                                                    self.teamIdDBCheck ()
-                                                    
-                                                    if fullName == "" || password == "" || email == "" || address1 == "" || address2 == "" || postcode == "" || teamId == "" {
-                                                        self.displayAlert(title: "Missing Information", message: "You must provide information in all of the fields provided.")
-                                                    } else {
-                                                        if teamIdExistsInDB {
-                                                            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
-                                                                if let error = error {
-                                                                    self.displayAlert(title: "Error", message: error.localizedDescription)
-                                                                } else {
-                                                                    let req = Auth.auth().currentUser?.createProfileChangeRequest()
-                                                                    req?.displayName = "Player"
-                                                                    req?.commitChanges(completion: nil)
-                                                                    
-                                                                    if let user = user {
-                                                                        let imageFolder = Storage.storage().reference().child("images")
-                                                                        if let uploadData = UIImageJPEGRepresentation(photo, 0.2) {
-                                                                            imageFolder.child("\(NSUUID().uuidString).jpg").putData(uploadData, metadata: nil, completion: { (metadata, error) in
-                                                                                if let error = error {
-                                                                                    self.displayAlert(title: "Error", message: error.localizedDescription)
-                                                                                }
-                                                                                if let profileImageUrl = metadata?.downloadURL()?.absoluteString  {
-                                                                                    
-                                                                                    //Add the player to the DB
-                                                                                    let playerDictionary : [String:Any] = ["Email": user.email!, "Full Name": fullName, "Address Line 1": address1, "Address Line 2": address2, "Postcode": postcode, "Team ID": teamId, "ProfileImage": profileImageUrl, "Manager": false, "Position":"Defender", "Position Side": "Right"]
-                                                                                    
-                                                                                    Database.database().reference().child("Players").child(user.uid).setValue(playerDictionary)
-                                                                                }
-                                                                            })
-                                                                        }
-                                                                        
-                                                                        
-                                                                    }
-                                                                }
-                                                            })
-                                                        } else {
-                                                            // Team is not in the db so users can't sign up
-                                                            self.displayAlert(title: "Team ID Error", message: "The team does not exist, please contact your manager to get a valid team")
-                                                        }
-                                                        //Segue back to the login page
-                                                        self.performSegue(withIdentifier: "signupSubmitSegue", sender: email)
-                                                    }
-                                                }
-                                            }
+        guard let photo = imgProfileImage.image, let email = tfEmailAddress.text, let password = tfPassword.text, let fullName = tfFullName.text, let address1 = tfAddressLine1.text, let address2 = tfAddressLine2.text, let postcode = tfPostcode.text, let teamId = tfTeamId.text, let teamName = tfTeamName.text, let teamPostcode = tfTeamPostcode.text else {
+            print("Something is missing?")
+            return
+        }
+        if self.managerSwitch.isOn {
+            //MANAGER MODE
+            if fullName == "" || password == "" || email == "" || address1 == "" || address2 == "" || postcode == "" || teamName == "" || teamPostcode == "" {
+                self.displayAlert(title: "Missing Information", message: "You must provide information in all of the fields provided.")
+            } else {
+                
+                teamNameDBCheck()
+                PostcodeDBCheck()
+                
+                if teamNameExistsInDB && teamPostcodeExistsInDB {
+                    self.displayAlert(title: "Team already exists", message: "The team already exists. Please create a different team or contact your club to get the TeamID.")
+                } else {
+                    
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if let error = error {
+                            self.displayAlert(title: "Error", message: error.localizedDescription)
+                        } else {
+                            let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                            req?.displayName = "Manager"
+                            req?.commitChanges(completion: nil)
+                            
+                            if let user = user {
+                                let imageFolder = Storage.storage().reference().child("images")
+                                if let uploadData = UIImageJPEGRepresentation(photo, 0.2) {
+                                    imageFolder.child("\(NSUUID().uuidString).jpg").putData(uploadData, metadata: nil, completion: { (metadata, error) in
+                                        if let error = error {
+                                            self.displayAlert(title: "Error", message: error.localizedDescription)
                                         }
-                                    }
+                                        if let profileImageUrl = metadata?.downloadURL()?.absoluteString  {
+                                            
+                                            //Add the Team to the DB
+                                            let newRef = Database.database().reference().child("Teams").childByAutoId()
+                                            let newKey = newRef.key
+                                            let TeamDictionary : [String:Any] = ["Team Name": teamName, "Team Postcode":teamPostcode, "id": newKey]
+                                            newRef.setValue(TeamDictionary)
+                                            
+                                            //Add the player to the DB
+                                            let playerDictionary : [String:Any] = ["Email": email, "Full Name": fullName, "Address Line 1": address1, "Address Line 2": address2, "Postcode": postcode, "Team ID": newKey, "ProfileImage": profileImageUrl, "Manager": true, "Position":"Defender", "Position Side": "Right"]
+                                            
+                                            Database.database().reference().child("Players").child(user.uid).setValue(playerDictionary)
+                                            
+                                            //Segue back to the login page
+                                            self.performSegue(withIdentifier: "signupSubmitSegue", sender: email)
+                                        }
+                                    })
                                 }
                             }
                         }
-                    }
+                    })
+                }
+            }
+        } else {
+            //PLAYER MODE
+            self.teamIdDBCheck ()
+            
+            if fullName == "" || password == "" || email == "" || address1 == "" || address2 == "" || postcode == "" || teamId == "" {
+                self.displayAlert(title: "Missing Information", message: "You must provide information in all of the fields provided.")
+            } else {
+                if teamIdExistsInDB {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if let error = error {
+                            self.displayAlert(title: "Error", message: (error.localizedDescription))
+                        } else {
+                            let req = Auth.auth().currentUser?.createProfileChangeRequest()
+                            req?.displayName = "Player"
+                            req?.commitChanges(completion: nil)
+                            
+                            if let user = user {
+                                let imageFolder = Storage.storage().reference().child("images")
+                                if let uploadData = UIImageJPEGRepresentation(photo, 0.2) {
+                                    imageFolder.child("\(NSUUID().uuidString).jpg").putData(uploadData, metadata: nil, completion: { (metadata, error) in
+                                        if let error = error {
+                                            self.displayAlert(title: "Error", message: error.localizedDescription)
+                                        }
+                                        if let profileImageUrl = metadata?.downloadURL()?.absoluteString  {
+                                            
+                                            //Add the player to the DB
+                                            let playerDictionary : [String:Any] = ["Email": user.email!, "Full Name": fullName, "Address Line 1": address1, "Address Line 2": address2, "Postcode": postcode, "Team ID": teamId, "ProfileImage": profileImageUrl, "Manager": false, "Position":"Defender", "Position Side": "Right"]
+                                            
+                                            Database.database().reference().child("Players").child(user.uid).setValue(playerDictionary)
+                                            
+                                            //Segue back to the login page
+                                            self.performSegue(withIdentifier: "signupSubmitSegue", sender: email)
+                                        }
+                                    })
+                                }
+                                
+                                
+                            }
+                        }
+                    })
+                } else {
+                    // Team is not in the db so users can't sign up
+                    self.displayAlert(title: "Team ID Error", message: "The team does not exist, please contact your manager to get a valid team")
                 }
             }
         }
+        
+
+    }
+    
+    func displayAlert(title:String, message:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -293,11 +284,6 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
     }
     
-    func displayAlert(title:String, message:String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alertController, animated: true, completion: nil)
-    }
     
     //closes the keyboard when you touch white space
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
