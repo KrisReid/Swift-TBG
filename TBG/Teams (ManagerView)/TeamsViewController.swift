5d8b3214d98c811023cd78f6f4a38a481b19338a
“KrisReid” <krisreid_06@hotmail.com>
2019-05-06 21:57:35 +0100

    Changes to swipe down

--- TBG/Teams (ManagerView)/TeamsViewController.swift
+++ TBG/Teams (ManagerView)/TeamsViewController.swift
@@ -16,17 +16,16 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     let dataStore = DataStore.init()
     
     @IBOutlet weak var tvPlayers: UITableView!
-    @IBOutlet weak var lblTeamName: UILabel!
     @IBOutlet weak var lblTeamIdDescription: UILabel!
     @IBOutlet weak var lblTeamId: UILabel!
     @IBOutlet weak var lblTeamPINDescription: UILabel!
     @IBOutlet weak var lblTeamPIN: UILabel!
     @IBOutlet weak var btnShare: UIButton!
-    @IBOutlet weak var btnUpdatePIN: UIButton!
-    @IBOutlet weak var tfPIN: UITextField!
     @IBOutlet weak var vShare: UIView!
     @IBOutlet weak var vShareCover: UIView!
-    @IBOutlet weak var btnCloseShare: UIButton!
+    @IBOutlet weak var vSwipeDown: UIView!
+    
+    @IBOutlet weak var tvShare: UITableView!
     
     
     var allPlayers : [DataSnapshot] = []
@@ -34,40 +33,25 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     var teamId : String = ""
     var teamPIN: Int = 0
-    var PINEditMode = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         _ = TokenGenerationViewController().viewDidLoad()
 
-        
+        //Refresh
         refresher.attributedTitle = NSAttributedString(string: "Pull to refresh")
         refresher.addTarget(self, action:
             #selector(TeamsViewController.updatePlayers), for: UIControl.Event.valueChanged)
         tvPlayers.addSubview(refresher)
         
-        btnShare.layer.cornerRadius = 5.0
-        setTextFields(textfieldName: tfPIN)
+        //Styling
+        styling()
         
-        vShare.layer.cornerRadius = CGFloat(10)
-        
-        vShare.frame = CGRect(x: 0 , y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2.5)
-        
-        vShareCover.frame = CGRect(x: 0 , y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        vShareCover.alpha = 0
-        
-        btnCloseShare.frame = CGRect(x: vShare.bounds.width - btnCloseShare.bounds.width, y: 0, width: 30, height: 30)
-        lblTeamName.frame = CGRect(x: 10, y: 0, width: vShare.bounds.width - btnCloseShare.bounds.width, height: 30)
-        lblTeamIdDescription.frame = CGRect(x: 10, y: 60, width: 70, height: 30)
-        lblTeamId.frame = CGRect(x: lblTeamIdDescription.frame.width + 20, y: 60, width: vShare.bounds.width - lblTeamIdDescription.frame.width + 20, height: 30)
-        
-        lblTeamPINDescription.frame = CGRect(x: 10, y: 90, width: 70, height: 30)
-        lblTeamPIN.frame = CGRect(x: lblTeamPINDescription.frame.width + 20, y: 90, width: vShare.bounds.width - lblTeamPINDescription.frame.width - 80, height: 30)
-        tfPIN.frame = CGRect(x: lblTeamPINDescription.frame.width + 20, y: 90, width: vShare.bounds.width - lblTeamPINDescription.frame.width - 80, height: 30)
-        btnUpdatePIN.frame = CGRect(x: lblTeamPINDescription.frame.width + 20 + lblTeamPIN.frame.width + 20, y: 90, width: 21, height: 21)
-        
-        btnShare.frame = CGRect(x: vShare.frame.width / 2 - 50, y: 170, width: 100, height: 30)
+        //Downswipe (Add Fixture)
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        downSwipe.direction = .down
+        vShareCover.addGestureRecognizer(downSwipe)
         
         //Keyboard show and dismiss observers
         NotificationCenter.default.addObserver(self, selector: #selector(SignUpViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -77,6 +61,27 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     override func viewDidAppear(_ animated: Bool) {
         getPlayers ()
+    }
+    
+    func styling () {
+        vShare.layer.cornerRadius = CGFloat(10)
+        vShare.frame = CGRect(x: 0 , y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2.5)
+        
+        vShareCover.frame = CGRect(x: 0 , y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        vShareCover.alpha = 0
+        
+        vSwipeDown.layer.cornerRadius = CGFloat(5)
+        vSwipeDown.frame = CGRect(x: UIScreen.main.bounds.width / 2 - vSwipeDown.frame.width / 2, y: UIScreen.main.bounds.height - 20, width: 48, height: 8)
+        vSwipeDown.alpha = 0
+        
+        lblTeamIdDescription.frame = CGRect(x: 10, y: 60, width: 70, height: 30)
+        lblTeamId.frame = CGRect(x: lblTeamIdDescription.frame.width + 20, y: 60, width: vShare.bounds.width - lblTeamIdDescription.frame.width + 20, height: 30)
+        
+        lblTeamPINDescription.frame = CGRect(x: 10, y: 90, width: 70, height: 30)
+        lblTeamPIN.frame = CGRect(x: lblTeamPINDescription.frame.width + 20, y: 90, width: vShare.bounds.width - lblTeamPINDescription.frame.width - 80, height: 30)
+        
+        btnShare.frame = CGRect(x: vShare.frame.width / 2 - 50, y: 170, width: 100, height: 30)
+        
     }
     
     //SMS CODE
@@ -99,23 +104,36 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     
     @IBAction func btnShare(_ sender: Any) {
-        print("Share Me Half Modal")
-        
         UIView.animate(withDuration: 0.6) {
             self.vShare.frame = CGRect(x: 0 , y: UIScreen.main.bounds.height - self.vShare.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2.5)
+            self.vSwipeDown.frame = CGRect(x: UIScreen.main.bounds.width / 2 - self.vSwipeDown.frame.width / 2, y: UIScreen.main.bounds.height - self.vShare.bounds.height - 20, width: 48, height: 8)
             self.vShare.alpha = 1
             self.vShareCover.alpha = 1
+            self.vSwipeDown.alpha = 1
         }
-        
     }
     
-    @IBAction func btnCloseShareClicked(_ sender: Any) {
-        UIView.animate(withDuration: 0.6) {
-            self.vShare.frame = CGRect(x: 0 , y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2.5)
-            self.vShare.alpha = 0
-            self.vShareCover.alpha = 0
+    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
+        
+        if (sender.direction == .down) {
+            print("Swipe Down")
+            UIView.animate(withDuration: 0.6) {
+                self.vShare.frame = CGRect(x: 0 , y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2.5)
+                self.vSwipeDown.frame = CGRect(x: UIScreen.main.bounds.width / 2 - self.vSwipeDown.frame.width / 2, y: UIScreen.main.bounds.height - 20, width: 48, height: 8)
+                self.vShare.alpha = 0
+                self.vShareCover.alpha = 0
+                self.vSwipeDown.alpha = 0
+            }
         }
     }
+    
+//    @IBAction func btnCloseShareClicked(_ sender: Any) {
+//        UIView.animate(withDuration: 0.6) {
+//            self.vShare.frame = CGRect(x: 0 , y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2.5)
+//            self.vShare.alpha = 0
+//            self.vShareCover.alpha = 0
+//        }
+//    }
     
     
     @IBAction func btnLogOut(_ sender: Any) {
@@ -123,33 +141,33 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         navigationController?.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func btnUpdatePINClicked(_ sender: Any) {
-        if PINEditMode {
-            tfPIN.isHidden = false
-            lblTeamPIN.isHidden = true
-            btnUpdatePIN.setImage(#imageLiteral(resourceName: "upload.png"), for: .normal)
-            self.PINEditMode = false
-        } else {
-//            if tfPIN.text?.characters.count == 6 {
-            if tfPIN.text?.count == 6 {
-                tfPIN.isHidden = true
-                lblTeamPIN.isHidden = false
-                lblTeamPIN.text = tfPIN.text
-                
-                btnUpdatePIN.setImage(#imageLiteral(resourceName: "edit.png"), for: .normal)
-                self.PINEditMode = true
-                
-                Database.database().reference().child("Teams").queryOrdered(byChild: "id").queryEqual(toValue: teamId).observe(.childAdded, with: { (snapshot) in
-
-                    print(Int(self.teamPIN))
-                    snapshot.ref.updateChildValues(["PIN": Int(self.tfPIN.text!)!])
-                    Database.database().reference().child("Teams").removeAllObservers()
-                })
-                
-                self.view.endEditing(true)
-            }
-        }
-    }
+//    @IBAction func btnUpdatePINClicked(_ sender: Any) {
+//        if PINEditMode {
+//            tfPIN.isHidden = false
+//            lblTeamPIN.isHidden = true
+//            btnUpdatePIN.setImage(#imageLiteral(resourceName: "upload.png"), for: .normal)
+//            self.PINEditMode = false
+//        } else {
+////            if tfPIN.text?.characters.count == 6 {
+//            if tfPIN.text?.count == 6 {
+//                tfPIN.isHidden = true
+//                lblTeamPIN.isHidden = false
+//                lblTeamPIN.text = tfPIN.text
+//
+//                btnUpdatePIN.setImage(#imageLiteral(resourceName: "edit.png"), for: .normal)
+//                self.PINEditMode = true
+//
+//                Database.database().reference().child("Teams").queryOrdered(byChild: "id").queryEqual(toValue: teamId).observe(.childAdded, with: { (snapshot) in
+//
+//                    print(Int(self.teamPIN))
+//                    snapshot.ref.updateChildValues(["PIN": Int(self.tfPIN.text!)!])
+//                    Database.database().reference().child("Teams").removeAllObservers()
+//                })
+//
+//                self.view.endEditing(true)
+//            }
+//        }
+//    }
     
     func setTextFields (textfieldName : UITextField) {
         let myColor = UIColor.gray
@@ -243,7 +261,6 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             if let TeamDictionary = snapshot.value as? [String:Any] {
                 if let teamName = TeamDictionary["Team Name"] as? String, let PIN = TeamDictionary["PIN"] as? Int {
                     
-                    self.lblTeamName.text = teamName
                     self.lblTeamPIN.text = String(PIN)
                     self.teamPIN = PIN
                 }
@@ -253,47 +270,51 @@ class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allPlayers.count
+//        return allPlayers.count
+        return tableView == tvPlayers ? allPlayers.count : 2
     }
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? PlayersTableViewCell {
-            
-            let snapshot = allPlayers[indexPath.row]
-            
-            if let PlayerDictionary = snapshot.value as? [String:Any] {
-                
-                if let fullName = PlayerDictionary["Full Name"] as? String, let imageURL = PlayerDictionary["ProfileImage"] as? String, let position = PlayerDictionary["Position"] as? String, let side = PlayerDictionary["Position Side"] as? String {
+        if tableView == tvPlayers {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? PlayersTableViewCell {
+                let snapshot = allPlayers[indexPath.row]
+                if let PlayerDictionary = snapshot.value as? [String:Any] {
+                    if let fullName = PlayerDictionary["Full Name"] as? String, let imageURL = PlayerDictionary["ProfileImage"] as? String, let position = PlayerDictionary["Position"] as? String, let side = PlayerDictionary["Position Side"] as? String {
                         
-                    cell.lblFullName.text = fullName
-                    cell.lblPosition.text = position
-                    cell.lblSide.text = side
-                    
-                    let url = URL(string: imageURL)
-                    let request = NSMutableURLRequest(url: url!)
-                    
-                    let task = URLSession.shared.dataTask(with: request as URLRequest) {
-                        data, response, error in
+                        cell.lblFullName.text = fullName
+                        cell.lblPosition.text = position
+                        cell.lblSide.text = side
                         
-                        if error != nil {
-                            print(error ?? "Error")
-                        } else {
-                            if let data = data {
-                                DispatchQueue.main.async {
-                                    if let image = UIImage(data: data) {
-                                        cell.ivProfilePic.image = image
+                        let url = URL(string: imageURL)
+                        let request = NSMutableURLRequest(url: url!)
+                        
+                        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                            data, response, error in
+                            
+                            if error != nil {
+                                print(error ?? "Error")
+                            } else {
+                                if let data = data {
+                                    DispatchQueue.main.async {
+                                        if let image = UIImage(data: data) {
+                                            cell.ivProfilePic.image = image
+                                        }
                                     }
                                 }
                             }
                         }
+                        task.resume()
+                        return cell
                     }
-                    task.resume()
-                    return cell
                 }
             }
+        } else {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as? ShareTableViewCell {
+                return cell
+            }
         }
+        
         return UITableViewCell()
     }
     
